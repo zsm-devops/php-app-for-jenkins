8398d590aa42642171f92730b267a948077ef456
SerhZavh <sfsd@afdasd.fsdf>
2020-07-28 17:06:00 +0300

    Version 3.0

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1><font color=magenta>Congratulations!</h1>
+        <h1><font color=magenta>Congratulations!<br> Mother Fycker</font></h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
